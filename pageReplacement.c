e5ba9165453e1c4c30872c90749d47bce7b5ec37
Johns Raju <83854933+JOHNSRAJU@users.noreply.github.com>
2022-10-18 20:16:02 +0530

    Update pageReplacement.c

--- pageReplacement.c
+++ pageReplacement.c
@@ -38,18 +38,10 @@ int main(){
 void fifo(int p[20],int n,int f){
 	int i,j,pageFault=0,frames[10],flag,point=0;
 	for(i=0;i<f;i++){
-		frames[i]=p[i];
-		pageFault++;
+		frames[i]=-1;
 	}
 	printf("\nREF\t  Frames\n");
-	for(i=0;i<f;i++){
-	printf("%d\t",p[i]);
-		for(j=0;j<=i;j++){
-			printf("%d   ",frames[j]);
-		}
-		printf("\n");
-	}
-	for(i=f;i<n;i++){
+	for(i=0;i<n;i++){
 		flag=0;
 		for(j=0;j<f;j++){
 			if(frames[j]==p[i]){
@@ -67,7 +59,9 @@ void fifo(int p[20],int n,int f){
 		
 		
 		for(j=0;j<f;j++){
-			printf("%d   ",frames[j]);
+            if(frames[j]!=-1){
+                printf("%d   ",frames[j]);
+            }
 		}
 		printf("\n");
 	}
@@ -75,49 +69,38 @@ void fifo(int p[20],int n,int f){
 }
 
 void lru(int p[20],int n,int f){
-	int i,j,pageFault=0,frames[10],flag,point,fla[10],k;
+	int i,j,min,pageFault=0,frames[10],flag,point,order[10];
 	for(i=0;i<f;i++){
-		frames[i]=p[i];
-		pageFault++;
+		frames[i]=-1;
+        order[i]=-1;
 	}
 	printf("\nREF\t  Frames\n");
-	for(i=0;i<f;i++){
-	printf("%d\t",p[i]);
-		for(j=0;j<=i;j++){
-			printf("%d   ",frames[j]);
-		}
-		printf("\n");
-	}
-	for(i=f;i<n;i++){
+	for(i=0;i<n;i++){
 		flag=0;
 		for(j=0;j<f;j++){
 			if(frames[j]==p[i]){
 				flag=1;
+                order[j]=i;
 				printf("%d *\t",p[i]);
 				break;
 			}
-			fla[j]=0;
 		}
 		if(flag==0){
-			for(j=i;j>i-f;j--){
-				for(k=0;k<f;k++){
-					if(p[j]==frames[k]){
-						fla[k]=1;
-					}
-				}
-			}
+            min=0;
 			for(j=0;j<f;j++){
-				if(fla[j]==0){
-					point=j;
-					break;
+				if(order[j]<order[min]){
+					min=j;
 				}
 			}
-			frames[point]=p[i];
-			pageFault++;
+			frames[min]=p[i];
+			order[min]=i;
 			printf("%d\t",p[i]);
+            pageFault++;
 		}
 		for(j=0;j<f;j++){
-			printf("%d   ",frames[j]);
+            if(frames[j]!=-1){
+                printf("%d   ",frames[j]);
+            }
 		}
 		printf("\n");
 	}
@@ -126,19 +109,11 @@ void lru(int p[20],int n,int f){
 void lfu(int p[20],int n,int f){
 	int i,j,k,pageFault=0,frames[10],flag,point=0,freq[10],min;
 	for(i=0;i<f;i++){
-		frames[i]=p[i];
-		freq[i]=1;
-		pageFault++;
+		frames[i]=-1;
+        freq[i]=0;
 	}
 	printf("\nREF\t  Frames\n");
-	for(i=0;i<f;i++){
-	printf("%d\t",p[i]);
-		for(j=0;j<=i;j++){
-			printf("%d   ",frames[j]);
-		}
-		printf("\n");
-	}
-	for(i=f;i<n;i++){
+	for(i=0;i<n;i++){
 		flag=0;
 		for(j=0;j<f;j++){
 			if(frames[j]==p[i]){
@@ -164,13 +139,12 @@ void lfu(int p[20],int n,int f){
 			freq[min]=1;
 			printf("%d\t",p[i]);
 		}
-		
-		
 		for(j=0;j<f;j++){
-			printf("%d   ",frames[j]);
+            if(frames[j]!=-1){
+                printf("%d   ",frames[j]);
+            }
 		}
 		printf("\n");
 	}
 	printf("No. of Page Fault :- %d",pageFault);
 }
-
